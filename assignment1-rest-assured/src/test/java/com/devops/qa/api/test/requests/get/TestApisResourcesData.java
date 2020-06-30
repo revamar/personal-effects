587a9df3d932e65643fc7c751cadd614dbb06625
revamar <selenium.xpert@gmail.com>
2020-06-30 11:44:03 +0530

    clean more codes

--- assignment1-rest-assured/src/test/java/com/devops/qa/api/test/requests/get/TestApisResourcesData.java
+++ assignment1-rest-assured/src/test/java/com/devops/qa/api/test/requests/get/TestApisResourcesData.java
@@ -10,9 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import io.restassured.specification.RequestSpecification;
-import com.devops.qa.api.test.reports.*;
 
-//@Listeners({ExtentITestListenerAdapter.class})
 public class TestApisResourcesData{
 	RequestSpecification request;
 	Response response;
--- assignment1-rest-assured/src/test/java/com/devops/qa/api/test/requests/get/TestApisVersions.java
+++ assignment1-rest-assured/src/test/java/com/devops/qa/api/test/requests/get/TestApisVersions.java
@@ -7,7 +7,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-//@Listeners({ExtentITestListenerAdapter.class})
 public class TestApisVersions{
 
 	RequestSpecification request;
